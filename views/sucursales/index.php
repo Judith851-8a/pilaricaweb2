0e30ae7b54a6d0a31a83ca85256f0450f3eb86e3
EdniMorales <rimora.29@gmail.com>
2026-04-09 13:31:43 -0600

    ajusta márgenes y padding de la clase 'fondo-sucursales' para mejorar el diseño de la sección

--- views/sucursales/index.php
+++ views/sucursales/index.php
@@ -1,7 +1,9 @@
-            <section class="fondo-sucursales fondo">
-                <div class="container px-5">
+            <section class="fondo-sucursales">
+                <div class="container">
                     <h1 class="title-sucursales dancing-script-bold wow animated fadeInDown" data-wow-delay="2s">  - Sucursales -</h1>
-                    <div class="card border-0 shadow rounded-3 overflow-hidden">
+
+                    <iframe width="100%" height="1020" src="https://lookerstudio.google.com/embed/reporting/3a3938c9-65a0-4045-83ca-dc05d39abcf3/page/UshuF" frameborder="0" style="border:0" allowfullscreen sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox"></iframe>
+                <!--     <div class="card border-0 shadow rounded-3 overflow-hidden">
                         <div class="card-body p-0">
                             <div class="row gx-0">
                             <div class="col-lg-12 col-xl-12">
@@ -12,6 +14,6 @@
                             </div>
                             </div>
                         </div>
-                    </div>
+                    </div> -->
                 </div>
             </section>
