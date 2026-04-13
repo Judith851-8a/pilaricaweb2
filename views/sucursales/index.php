b2ef0b2b6d808f2ee93f5aa51434e3c64d973e32
Judith851-8a <karla8ajr@gmail.com>
2026-04-13 09:49:58 -0600

    Actualización de link mapa

--- views/sucursales/index.php
+++ views/sucursales/index.php
@@ -2,7 +2,7 @@
                 <div class="container">
                     <h1 class="title-sucursales dancing-script-bold wow animated fadeInDown" data-wow-delay="2s">  - Sucursales -</h1>
 
-                    <iframe width="100%" height="1020" src="https://lookerstudio.google.com/embed/reporting/3a3938c9-65a0-4045-83ca-dc05d39abcf3/page/UshuF" frameborder="0" style="border:0" allowfullscreen sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox"></iframe>
+                    <iframe width="100%" height="1020" src="https://lookerstudio.google.com/embed/reporting/88158e8a-0355-4d32-b6d6-ec66ee786fe4/page/aVouF" frameborder="0" style="border:0" allowfullscreen sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox"></iframe>
                 <!--     <div class="card border-0 shadow rounded-3 overflow-hidden">
                         <div class="card-body p-0">
                             <div class="row gx-0">
